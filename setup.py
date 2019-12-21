2ee7e2ec37e5894af896ef61583d8ab5eb027c96
Jordi Reig <jordireigcallis@gmail.com>
2019-12-21 13:46:32 +0100

    Create project with old repository, ported to Python 3

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from setuptools import setup
+from glob import glob
+
+version = "1.0"
+setup(
+    name='jutge-problems-toolkit',
+    packages=['jutge_problems_toolkit'],
+    install_requires=['pyyaml'],
+    version=version,
+    description='Toolkit to create problems for Jutge.org',
+    long_description='Toolkit to create problems for Jutge.org',
+    author='Jordi Petit et al',
+    author_email='jpetit@cs.upc.edu',
+    url='https://github.com/jutge-org/jutge-problems-toolkit',
+    download_url='https://github.com/jutge-org/jutge-problems-toolkit/tarball/{}'.format(version),
+    keywords=['jutge', 'jutge.org', 'education', 'problems', 'toolkit'],
+    license='Apache',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Education',
+    ],
+    zip_safe=False,
+    data_files=[('jutge_problems_toolkit/sty', glob('jutge_problems_toolkit/sty/*'))],
+    setup_requires=['setuptools'],
+    entry_points={'console_scripts': ['jutge-problems-toolkit=jutge_problems_toolkit:main']}
+)
